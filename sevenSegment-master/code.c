1d4c2dc817451688ccdf420cf8f98ed78d57834c
Adarsh Gajula <82866381+Adarsh2425@users.noreply.github.com>
2024-01-26 16:15:22 +0530

    Update code.c

--- sevenSegment-master/code.c
+++ sevenSegment-master/code.c
@@ -1,5 +1,4 @@
 #include 
-//****** Milad Samani - 962059895 ********
 int i=0, l ,k;
 unsigned char segment[10]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
 int main (void){
@@ -13,5 +12,4 @@ IO0CLR |= (segment[i])<<4;
 i++;
 if(i==10)i=0;}
 }
-//****** Milad Samani - 962059895 ********
 
